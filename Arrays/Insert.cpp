48d03ee523f29566653e90107112da0dd21ab018
foosayyy <dankrish26@gmail.com>
2021-12-12 11:22:30 -0800

    Learning arrays

--- Arrays/Insert.cpp
+++ Arrays/Insert.cpp
@@ -1,4 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main();
+void insert(int arr[],int cap,int n, int x){
+    if(cap = n){
+        return cap;
+    }
+    
+
+}
+
+int main(){
+    int cap,x;
+    int n = 5;
+    int arr[n] = {1,2,3,};
+
+
+    return 0;
+}
