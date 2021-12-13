9e3e068ddbe4a5f441a8a5d2a9ad63641ab1c3ba
foosayyy <dankrish26@gmail.com>
2021-12-13 21:10:07 +0530

    operation on array

--- Arrays/Insert.cpp
+++ Arrays/Insert.cpp
@@ -1,19 +1,22 @@
+//
+// Created by dankr on 13-12-2021.
+//
+
 #include <bits/stdc++.h>
 using namespace std;
 
-void insert(int arr[],int cap,int n, int x){
-    if(cap = n){
-        return cap;
+int search(int arr[], int n, int x){
+    for(int i=0; i<n; i++) {
+        if (arr[i] == x)
+            return i;
     }
-    
-
+    return -1;
 }
 
 int main(){
-    int cap,x;
-    int n = 5;
-    int arr[n] = {1,2,3,};
-
-
+    int arr[5] = {20,5,7,25};
+    int x = 5;
+    int sol = search(arr,4,x);
+    cout<<sol;
     return 0;
 }
