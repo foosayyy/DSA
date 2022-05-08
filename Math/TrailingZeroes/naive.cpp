3d034f027cc36eac00387080a618096bc36f760d
foosayyy <85921796+foosayyy@users.noreply.github.com>
2022-05-08 09:43:50 +0530

    Create naive.cpp

---
+++ Math/TrailingZeroes/naive.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int trailingzero(int n){
+	int fact = 1;
+	for(int i=2; i<=n; i++){
+		fact = fact*i;
+	}
+	int res=0;
+	while(fact%10==0){
+		res++;
+		fact = fact/10;
+	}
+	return res;
+}
+
+int main(){
+	int x;
+	cin>>x;
+	cout<<trailingzero(x);
+	return 0;
+}
